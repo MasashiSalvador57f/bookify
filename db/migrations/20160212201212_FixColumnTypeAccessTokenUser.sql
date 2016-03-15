7e6d3ccd3dac79569c66a85542a2882b4f7bf930
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2016-03-15 21:22:41 +0900

    add migration files for access_token

--- db/migrations/20160212201212_FixColumnTypeAccessTokenUser.sql
+++ db/migrations/20160212201212_FixColumnTypeAccessTokenUser.sql
@@ -1,8 +1,8 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE `user` MODIFY COLUMN access_token VARCHAR(64);
+ALTER TABLE `user` CHANGE COLUMN access_token access_token VARCHAR(64);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE `user` MODIFY COLUMN access_token VARCHAR(40);
+ALTER TABLE `user` CHANGE COLUMN access_token access_token VARCHAR(40);
--- db/migrations/20160315211721_AddUniqueIndexToUserAccessToken.sql
+++ db/migrations/20160315211721_AddUniqueIndexToUserAccessToken.sql
@@ -1,8 +1,8 @@
-ALTER TABLE user ADD COLUMN access_token VARCHAR(40)
+
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-
+ALTER TABLE `user` ADD UNIQUE index_on_access_token (`access_token`)
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE user DROP COLUMN access_token
+ALTER TABLE `user` DROP INDEX index_on_access_token (`access_token`)
