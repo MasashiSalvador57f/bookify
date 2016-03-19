8093d0f2b109933c9c78b5f6b63741c2f2380bfa
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2016-03-19 15:21:03 +0900

    add paserTime=true

--- app/shared/database_accessor/database_accessor.go
+++ app/shared/database_accessor/database_accessor.go
@@ -16,7 +16,7 @@ func InitDB(c *gin.Context) {
 	dbPass := "hogehoge"
 	dbProtocol := "tcp"
 	dbName := "bookify_development"
-	dsn := fmt.Sprintf("%s:%s@%s/%s", dbUser, dbPass, dbProtocol, dbName)
+	dsn := fmt.Sprintf("%s:%s@%s/%s?parseTime=true", dbUser, dbPass, dbProtocol, dbName)
 
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
@@ -26,7 +26,8 @@ func InitDB(c *gin.Context) {
 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.MySQLDialect{}}
 
 	// add definitions of tables to dbmap
-	dbmap.AddTableWithName(model.User{}, "user").SetKeys(true, "id")
+	t := dbmap.AddTableWithName(model.User{}, "user").SetKeys(true, "id")
+	t.ColMap("FacebookID").SetUnique(true)
 	c.Set("dbmap", dbmap)
 }
 
